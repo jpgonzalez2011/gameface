94b0d00902d1495c38f7889efd7e8434609d7216
JP <jp.gonzalez2011@gmail.com>
2016-01-26 12:30:50 -0500

    Fix bug in user sign in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,10 @@ class User < ActiveRecord::Base
     self.password_digest = BCrypt::Password.create(password)
   end
 
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
   def reset_session_token!
     self.session_token = SecureRandom::urlsafe_base64(16)
     self.save!
