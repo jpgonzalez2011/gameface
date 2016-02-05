b6953ebe07fde5002cfb41b3c3c6aa962914521b
JP <jp.gonzalez2011@gmail.com>
2016-02-05 02:22:54 -0500

    Continue work on friend ratings.

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -2,14 +2,14 @@ class Api::FriendshipsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @friends = @user.friends
+    @friends = @user.friends.sort { |x,y| x[:rating] <=> y[:rating] }
   end
 
   def update_rating()
-    debugger
-    @first_friendship = Friendship.where("requested_friend = :first and received_friend = :second", { first: params[:firstFriend], second: params[:secondFriend] }).to_a
-    @second_friendship = Friendship.where("requested_friend = :second and received_friend = :first", { first: params[:firstFriend], second: params[:secondFriend] }).to_a
+    @first_friendship = Friendship.where("requested_friend = :first and received_friend = :second", { first: User.find(params[:firstFriend][:id]), second: User.find(params[:secondFriend][:id]) }).to_a
+    @second_friendship = Friendship.where("requested_friend = :second and received_friend = :first", { first: User.find(params[:firstFriend][:id]), second: User.find(params[:secondFriend][:id])}).to_a
     @friendship = @first_friendship[0] || @second_friendship[0]
+    debugger
     if @friendship
       @friendship[:rating] += 1
       @friendship.save!
