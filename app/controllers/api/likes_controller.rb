404a9ce154c3ab69b8b4913bde1b70acdb89eedb
JP <jp.gonzalez2011@gmail.com>
2016-02-25 11:32:31 -0500

    finish create method on likes controller

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -3,10 +3,9 @@ class Api::LikesController < ApplicationController
   def create
     @like = Like.new(like_params)
     if @like.liker_id == current_user.id && @like.save
-      render json: {}
+      render json: {@like.likeable}
     else
       render json: {}
     end
   end
-
 end
