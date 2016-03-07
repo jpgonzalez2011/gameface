9597446d259c47a949fa86a431201f0b84f74b7d
JP <jp.gonzalez2011@gmail.com>
2016-03-06 21:31:08 -0500

    Optimize timeline query.

--- app/controllers/api/timeline_controller.rb
+++ app/controllers/api/timeline_controller.rb
@@ -3,8 +3,7 @@ class Api::TimelineController < ApplicationController
   def index
     @friend_ids = current_user.friends.map {|friend| friend.id }
     @timeline_ids = @friend_ids.push(current_user.id)
-    @target_posts = Post.includes({ poster: :profile_picture }, :target, comments: { commenter: :profile_picture }).where(target_id: @timeline_ids)
-    @poster_posts = Post.includes({ poster: :profile_picture }, :target, comments: { commenter: :profile_picture }).where(poster_id: @timeline_ids)
+    @target_posts = Post.includes({ poster: :profile_picture }, :target, comments: { commenter: :profile_picture }).where("target_id IN (?) OR poster_id IN (?)", @timeline_ids, @timeline_ids)
     @posts = (@target_posts.to_a + @poster_posts.to_a).uniq { |post| post.id }
     @photos = Photo.includes({uploader: :profile_picture}, comments: {commenter: :profile_picture}).where(uploader_id: @timeline_ids)
     @unsorted_timeline = @posts + @photos
