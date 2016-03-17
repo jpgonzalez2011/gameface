a1925812f2162160344cdcb3a84b0cd848c6f542
JP <jp.gonzalez2011@gmail.com>
2016-03-16 23:52:36 -0400

    Adjust paperclip options for profile photos.

--- app/models/profile_picture.rb
+++ app/models/profile_picture.rb
@@ -1,7 +1,17 @@
 class ProfilePicture < ActiveRecord::Base
   validates :user_id, presence: true, uniqueness: true
 
-  has_attached_file :image, styles: { medium: "172x172>", small: "100x100>", thumb: "30x30>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image,
+                    styles: {
+                      medium: "172x172#",
+                      small: "100x100#",
+                      thumb: "30x30#"
+                    }, default_url: "/images/:style/missing.png",
+                    convert_options: {
+                      thumb: "-quality 75 -strip",
+                      small: "-quality 75 -strip",
+                      medium: "-quality 75 -strip"
+                    }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
