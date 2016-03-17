2f98786478d24b39c733236d41555e82644baa36
JP <jp.gonzalez2011@gmail.com>
2016-03-16 23:55:44 -0400

    Adjust paperclip options for cover photos.

--- app/models/cover_photo.rb
+++ app/models/cover_photo.rb
@@ -1,6 +1,13 @@
 class CoverPhoto < ActiveRecord::Base
   validates :user_id, presence: true, uniqueness: true
-  has_attached_file :image, styles: { cover: "848x315>"}, default_url: "/images/:style/missing.png"
+  has_attached_file :image,
+                    styles: {
+                      cover: "848x315#"
+                    },
+                    convert_options: {
+                      cover: "-quality 75 -strip"
+                    }
+                    default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
